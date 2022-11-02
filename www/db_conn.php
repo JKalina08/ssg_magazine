3947079b0964aba4148bf9e427b086214067da31
JKarmasin <karmasin@student.vspj.cz>
2022-11-02 19:04:02 +0100

    new scl script

--- www/db_conn.php
+++ www/db_conn.php
@@ -1,9 +1,11 @@
 <?php
-$sname= "localhost";
+$sname= "localhost:3306";
 $uname= "root";
-$password = "";
+$password = "secret";
 $db_name = "ssg";
+
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
+//$conn = mysqli_connect($sname, $uname, $password, $db_name);
 
 if (!$conn) {
     echo "Connection failed!";
